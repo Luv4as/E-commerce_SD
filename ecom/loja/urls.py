0a28a2786d82839ab7d7ad5338c4c025b9b72f79
Lucas Venancio <lucas.venancio@outlook.com>
2024-02-11 21:06:38 -0300

    add: login e logout

--- ecom/loja/urls.py
+++ ecom/loja/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('/sobre_nos', views.about, name='sobre_nos'),
+    path('/login', views.login_user, name='login'),
+    path('/logout', views.logout_user, name='logout'),
 ]
--- ecom/loja/views.py
+++ ecom/loja/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Produto
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
     produtos = Produto.objects.all()
@@ -7,3 +9,24 @@ def home(request):
 
 def about(request):
     return render(request, 'sobre_nos.html', {})
+
+def login_user(request):
+    if  request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ('Você está logado'))
+            return redirect('home')
+
+        else:
+            messages.success(request, ('Houve um problema nos dados inseridos, tente novamente'))
+            return redirect('login')
+    else:
+        return render(request, 'login.html', {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ('Você deslogou da sua conta'))
+    return redirect('home')
